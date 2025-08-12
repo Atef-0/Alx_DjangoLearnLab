4541a80292f09ac83dfbd06d7b982531b1274213
Atef <mohammedatefezzat@gmail.com>
2025-08-12 21:51:14 +0300

    m1

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,16 +3,18 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField()
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 class Library(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
 
 class Ligrarian(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
-    
+
