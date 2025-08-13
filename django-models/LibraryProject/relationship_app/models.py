3742845ef6d2858f1335be3ebc70f8a798dfe70f
Atef <mohammedatefezzat@gmail.com>
2025-08-13 18:14:00 +0300

    m7

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,26 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
 class Author(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.name
-
+    
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+    
+    def __str__(self):
+        return self.title
 
 class Library(models.Model):
-    name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book)
-
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name="libraries")
+    
+    def __str__(self):
+        return self.name
+    
 class Librarian(models.Model):
-    name = models.CharField(max_length=200)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+    
+    def __str__(self):
+        return self.name
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
@@ -35,7 +52,7 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-# Signal to automatically create UserProfile after registeration
+# Signal to automatically create UserProfile when a new user is registered
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
@@ -47,15 +64,3 @@ def save_user_profile(sender, instance, **kwargs):
         instance.userprofile.save()
     else:
         UserProfile.objects.create(user=instance)
-
-
-## Task 04class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    class Meta:
-        permissions = [
-            ('can_add_book', 'Can add a book'),
-            ('can_change_book', 'Can change a book'),
-            ('can_delete_book', 'Can delete a book'),
-        ]
