bd1dd938fee2abcef287fede5a4ea2e0fd1e6bdb
Atef <mohammedatefezzat@gmail.com>
2025-08-14 19:57:09 +0300

    m1

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser, CustomUserManager
 
 # Register your models here.
 
@@ -8,5 +8,11 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)
     search_fields = ('title', 'author')
 
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "date_of_birth", "is_staff")
+    search_fields = ("username", "email")
+    list_filter = ("is_staff", "is_superuser")
+
 
 admin.site.register( Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -5,11 +5,6 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
