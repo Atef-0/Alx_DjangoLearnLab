05ef63ddb4a536bb1f3721b4ad7b28be4246eca6
Atef <mohammedatefezzat@gmail.com>
2025-08-14 20:36:16 +0300

    m1

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+
+
+
+
+
 # Create your models here.
-
-
 class Author(models.Model):
     name = models.CharField(max_length=200)
     def __str__(self):
