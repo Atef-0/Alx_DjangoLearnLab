2fba9be2e6ef1d44ac8528dc9488bbbaff5c412d
Atef <mohammedatefezzat@gmail.com>
2025-08-19 23:48:12 +0300

    task1 v1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from django.urls import reverse_lazy
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 # Create your views here.
+"""
+Implement a set of generic views for the Book model to handle CRUD operations. This includes:
+-A ListView for retrieving all books.
+-A DetailView for retrieving a single book by ID.
+-A CreateView for adding a new book.
+-An UpdateView for modifying an existing book.
+-A DeleteView for removing a book
+"""
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+    
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'id'
+    permission_classes = [AllowAny]
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    success_url = reverse_lazy('book-list')
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return self.get_success_response(serializer.data)
+        return self.get_error_response(serializer.errors)
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'id'
+    success_url = reverse_lazy('book-list')
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return self.get_success_response(serializer.data)
+        return self.get_error_response(serializer.errors)
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'id'
+    success_url = reverse_lazy('book-list')
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return self.get_success_response({'message': 'Book deleted successfully'})
