bde675bd14f3746cabfc7ade7e1996f3fcf5dd74
Atef <mohammedatefezzat@gmail.com>
2025-08-19 19:10:12 +0300

    m2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -35,19 +35,19 @@ def is_member(user):
 
 # Role-based views with both decorators
 
-@user_passes_test(is_admin, login_url='/login/')
+@user_passes_test(is_admin)
 def admin_view(request):
     """Admin-only view"""
     return render(request, 'relationship_app/admin_view.html')
 
 
-@user_passes_test(is_librarian, login_url='/login/')
+@user_passes_test(is_librarian)
 def librarian_view(request):
     """Librarian-only view"""
     return render(request, 'relationship_app/librarian_view.html')
 
 
-@user_passes_test(is_member, login_url='/login/')
+@user_passes_test(is_member)
 def member_view(request):
     """Member-only view"""
     return render(request, 'relationship_app/member_view.html')
